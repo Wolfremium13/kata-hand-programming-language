a13e338fd2cdaa0c566cf59ffbaa8cbdb491f063
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-11-30 18:14:16 +0000

    ❌ interpretate the instructions

--- src/tests/interpreter.test.ts
+++ src/tests/interpreter.test.ts
@@ -7,27 +7,40 @@ import { Interpreter } from '../core/interpreter';
 describe('The interpreter should', () => {
 	it('print letter A', () => {
 		const instructions = Instructions.from(
-			"👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👊"
-		)
-		const pointer = new Pointer()
-		const memory = new Memory(pointer)
-		const interpreter = new Interpreter(memory)
+			'👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👊'
+		);
+		const pointer = new Pointer();
+		const memory = new Memory(pointer);
+		const interpreter = new Interpreter(memory);
 
-		interpreter.execute(instructions)
+		interpreter.execute(instructions);
 
 		expect(interpreter.getBuffer()).toBe('A');
 	});
 
 	it('print letter B', () => {
 		const instructions = Instructions.from(
-			"👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👊"
-		)
-		const pointer = new Pointer()
-		const memory = new Memory(pointer)
-		const interpreter = new Interpreter(memory)
+			'👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👊'
+		);
+		const pointer = new Pointer();
+		const memory = new Memory(pointer);
+		const interpreter = new Interpreter(memory);
 
-		interpreter.execute(instructions)
+		interpreter.execute(instructions);
 
 		expect(interpreter.getBuffer()).toBe('B');
+	});
+
+	it('interpretate the instructions', () => {
+		const holaInstruction =
+			'👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👊👉👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👇👊👉👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👊👉👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👆👊';
+		const instructions = Instructions.from(holaInstruction);
+		const pointer = new Pointer();
+		const memory = new Memory(pointer);
+		const interpreter = new Interpreter(memory);
+
+		interpreter.execute(instructions);
+
+		expect(interpreter.getBuffer()).toBe('Hola');
 	});
 });
