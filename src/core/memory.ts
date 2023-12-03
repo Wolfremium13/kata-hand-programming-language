19f2fb48987baff5bc4988f27e21ef86d5a5907f
Wolfremium13 <wolfremiuminformatica@gmail.com>
2023-12-03 18:18:32 +0000

    ✅ go to the end when there are not more left positions

--- src/core/memory.ts
+++ src/core/memory.ts
@@ -1,22 +1,27 @@
-import { Instruction } from "./instructions";
-import { Pointer } from "./pointer";
+import { Instruction } from './instructions';
+import { Pointer } from './pointer';
 
-export class Memory{
-    private position = 0
+export class Memory {
+	private position = 0;
+	private memory: any[] = [];
 
-    constructor(pointer: Pointer){
-        
-    }
+	constructor(pointer: Pointer) {}
 
-    update(instruction: Instruction) {
-        if (instruction == "👉") {
-            this.position++
-        } else {
-            this.position--
-        }
-    }
+	update(instruction: Instruction) {
+		if (instruction == '👉') {
+			this.position++;
+			if (this.position >= this.memory.length) {
+				this.memory.push(0);
+			}
+		} else {
+			this.position--;
+		}
+		if (this.position < 0) {
+			this.position = this.memory.length;
+		}
+	}
 
-    getPointerPosition(): any {
-       return this.position
-    }
+	getPointerPosition(): any {
+		return this.position;
+	}
 }
